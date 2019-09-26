d1838c13abaf55616f74dca360419510237e5b75
Tri Phan <minhtri11tk@gmail.com>
2019-09-26 13:23:44 -0500

    Add gifs when answer right or wrong

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -43,17 +43,36 @@ $(".scoreContainer").hide();
 
 //Press button to start the program.
 function startButton() {
-  $("#start").on("click", function () 
-  {
+  $("#start").on("click", function () {
     timerInterval();
     $("button").hide();
   });
 }
 
+
 // a buffer function for setTimeInterval so there wont be an overlap in seconds.
 function buffer() {
-  //call timer function
+  //call timer function.
   timerInterval();
+}
+
+
+function imageDisplay(){
+  $(".onClickButton").hide();
+  $("#image").show();
+  var x = document.createElement("IMG");    
+  x.setAttribute("src", imagePic);
+  x.setAttribute("width", "304");
+  x.setAttribute("height", "228");
+  $("#image").html(x);
+
+  setTimeout(function(){
+    $("#image").hide();
+    if (b < questionObjects.length)
+    {
+      $(".onClickButton").show();
+    }    
+  }, 2000 );
 }
 
 //creating timecountdown.
@@ -61,25 +80,25 @@ function timerInterval() {
   var sec = 10;
   document.getElementById("timer").innerHTML = "Time Remain: " + sec + " second";
 
-  timerFinal = setInterval(function() {
+  timerFinal = setInterval(function () {
     sec--;
-    document.getElementById("timer").innerHTML = "Time Remain: " + sec + " second";  
+    document.getElementById("timer").innerHTML = "Time Remain: " + sec + " second";
     console.log(timerFinal);
 
-    if (sec == 0)     {
+    if (sec == 0) {
       notAnswer++;
       $("#guess").show();
       document.getElementById("guess").innerHTML = "Your out of time. The correct answer is: " + answerArray[b];
-
+      imageDisplay();
       a = a + 4;
-      b++; 
+      b++;
 
       clearInterval(timerFinal);
       console.log("timerInterval clear: " + timerFinal);
 
-      if (b >= questionObjects.length) {            
+      if (b >= questionObjects.length) {
         result();
-        return;  
+        return;
       }
 
       setTimeout(function () {
@@ -95,13 +114,13 @@ function timerInterval() {
 
 // use to initialize the Answer choices, questions and take their answer.
 function QA() {
-  
+
   // if running out of questions, call the result function to end the program.
   if (b >= questionObjects.length) {
     result();
     return;
   }
-  
+
   // hide the result 
   $("#guess").hide();
 
@@ -119,7 +138,7 @@ function QA() {
     document.getElementById("answerChoice4").innerHTML = "4. " + answerChoice[a + 3];
     document.getElementById("answerChoice4").value = answerChoice[a + 3];
   }
-  
+
   $("#answerChoice1, #answerChoice2, #answerChoice3, #answerChoice4").bind("click", function () {
 
     //unbind it after click
@@ -127,38 +146,43 @@ function QA() {
     if (this.value === answerArray[b]) {
       $("#guess").show();
       document.getElementById("guess").innerHTML = "You are correct!";
+      imagePic = "https://media.tenor.com/images/0640af817a95f0460657f52a98225fcd/tenor.gif";
+      imageDisplay();
       correctJ++;
     }
     else {
       $("#guess").show();
       document.getElementById("guess").innerHTML = "You are wrong! The correct answer is: " + answerArray[b];
+      imagePic = "https://i.imgur.com/Q375Y1I.gif?1";
+      imageDisplay();
       wrongJ++;
     }
-    
+
     // rotate to the next 4 answer choice
-    a = a + 4;  
+    a = a + 4;
     b++;
 
     //pause the time after we click
-    clearInterval(timerFinal);    
+    clearInterval(timerFinal);
     console.log("click function: " + timerFinal);
 
     // if out of question go to result;
-    if (b >= questionObjects.length) {      
+    if (b >= questionObjects.length) {
       result();
-      return;  
+      return;
     }
     //pause for 2 second then run buffer
     setTimeout(function () {
       buffer();
     }, 2000);
   });
-  
+
 }
 
-function result() {  
+function result() {
   $("#timer").hide();
   $(".onClickButton").hide();
+  $("#guess").hide();
   $(".scoreContainer").show();
   document.getElementById("correct").innerHTML = "Right answers: " + correctJ;
   document.getElementById("wrong").innerHTML = "Wrong answers: " + wrongJ;
